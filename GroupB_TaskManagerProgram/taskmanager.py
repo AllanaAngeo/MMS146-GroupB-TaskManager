bd6cd7b6e0cd1d0c62c785a30b9d2ade17ca90d1
Yana <151821017+AllanaAngeo@users.noreply.github.com>
2024-08-28 22:51:55 +0800

    Update taskmanager.py
    Added documentation in Task Manager class
    
    Task Manager class officially finished!

--- GroupB_TaskManagerProgram/taskmanager.py
+++ GroupB_TaskManagerProgram/taskmanager.py
@@ -2,27 +2,33 @@ from task import Task
 from datetime import date
 
 class TaskManager(Task):
-    task_list = []
+    '''
+    Task Manager class object based on the task class:
+       - also creates tasks using the same attributes
+       - adds, deletes, and edits tasks in a list
+       - also allows to view task list, sort said list according to priority level,
+         and display the overdue tasks
+    '''
+    task_list = [] #Task list for effective tasks
     def __init__(self, task_name, description, due_date):
-        super().__init__(task_name, description, due_date)
+        super().__init__(task_name, description, due_date) #Inherits Task class attributes and methods
 
-    def add_task (self, task_name):
-       if self.task_name == task_name:
+    def add_task (self, task_name): 
+       if self.task_name == task_name: #Checks if the task name is an attribute value of an instance
            task_info = {
                 "Name": self.task_name,
                 "Description": self.description,
                 "Due Date": self.due_date,
                 "Priority Level": self.priority_level,
-                "Completion Status": self.completion_status
-            }
-           TaskManager.task_list.append(task_info)
-           print(f"Task '{self.task_name}' added!")
-
-    #Not final, the code should look something like this (uses the functions of the task class for the edit_task function)
+                "Completion Status": self.completion_status #Creates a dictionary using the attributes of an instance
+            } 
+           TaskManager.task_list.append(task_info) #Adds dictionary to the task list
+           print(f"Task '{self.task_name}' added!") #Notifies if the method has been executed successfully
+           
     def edit_task (self, task_name, new_task_name=None, new_description=None, new_priority_level=None, new_completion_status=None, new_due_date=None):
-        for task in TaskManager.task_list: #finished edit method HIHI
-            if task["Name"] == task_name:
-                TaskManager.delete_task(task_name)
+        for task in TaskManager.task_list: #Use a for loop to go through each item (dictionary) of the task list
+            if task["Name"] == task_name: #Checks if the task name is present in an item's (dictionary) key "Name" value
+                TaskManager.delete_task(task_name) #Removes the matching dictionary
                 if new_task_name:
                     self.set_task_name(new_task_name)
                 if new_description:
@@ -32,7 +38,7 @@ class TaskManager(Task):
                 if new_completion_status:
                     self.set_completion_status(new_completion_status)
                 if new_due_date:
-                    self.set_due_date(new_due_date)
+                    self.set_due_date(new_due_date) #if any of the new_(attribute name) have value, it will call the method to change the attribute value
         updated_info = {
         "Name": self.task_name,
         "Description": self.description,
@@ -40,26 +46,27 @@ class TaskManager(Task):
         "Priority Level": self.priority_level,
         "Completion Status": self.completion_status
         }
-        TaskManager.task_list.append(updated_info)
-        print(f"Task '{self.task_name}' updated!")
+        TaskManager.task_list.append(updated_info) #Create new dictionary and add it again to the task list
+        print(f"Task '{self.task_name}' updated!") #Notifies if the method is successful
 
     def delete_task (task_name):
         TaskManager.task_list = [task for task in TaskManager.task_list if task["Name"] != task_name]
+        #Updates the task list to only include dictionaries whose key ["Name"] dont match with the task name
         print(f"Removed {task_name} from the list of tasks!")
 
     @classmethod
-    def view_all_tasks(cls): #this is like flawless very nyamers this will stay
+    def view_all_tasks(cls): #prints the contents of the list
         print(cls.task_list)
 
     @classmethod
     def sort_tasks_by_priority (cls):
-        cls.task_list.sort(key=lambda task: task['Priority Level'])
+        cls.task_list.sort(key=lambda task: task['Priority Level']) #Sorts the task list based on the dictionary key ["Priority Level"]
         print(f"Sorted tasks according to priority")
 
     def get_overdue_tasks ():
-        overdue_tasks = []
-        current_date = date.today()
-        for task in TaskManager.task_list:
+        overdue_tasks = [] #Create list for overdue tasks
+        current_date = date.today() #Store today's date
+        for task in TaskManager.task_list: #Checks if there are any due dates that are before today's date and adds them to the overdue list
             if task["Due Date"] < current_date:
                 overdue_tasks.append(task)
         print("Here are the overdue tasks:")
