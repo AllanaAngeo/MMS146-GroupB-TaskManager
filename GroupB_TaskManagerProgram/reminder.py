54eefc98b3a68cf83aadd041f6784e5542c50da2
Yana <151821017+AllanaAngeo@users.noreply.github.com>
2024-08-28 22:23:23 +0800

    Update reminder.py
    wanted to put documentation na sa reminder class but i took a look at it and i felt dumb so im leaving it to the ones na nagedit neto keke
    
    may iniwan naman akong format paano ;-; sorry po

--- GroupB_TaskManagerProgram/reminder.py
+++ GroupB_TaskManagerProgram/reminder.py
@@ -1,13 +1,17 @@
 from datetime import date
 from task import Task
-
+'''
+    Reminder class object based on the task class:
+       - also creates tasks
+       - has attributes... padagdag nalang arghhhh
+    '''
 class Reminder(Task):
-    def __init__(self, task: Task, notifications=""):
+    def __init__(self, task: Task, notifications=""): #This does something ganito
         self.task = task
         self.time_until_deadline = self.calculate_time_until_deadline()
         self.notifications = notifications
 
-    def calculate_time_until_deadline(self):
+    def calculate_time_until_deadline(self):#This does something ganire
         current_date = date.today()
         return self.task.due_date - current_date 
 
